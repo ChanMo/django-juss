649b1888756f5d242e90f530265f35b10a8e8ed9
dyou <chen.orange@aliyun.com>
2019-12-07 16:21:00 +0800

    add flatpages admin

--- admin.py
+++ admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.flatpages.admin import FlatPageAdmin
+from django.contrib.flatpages.models import FlatPage
+from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .widgets import RichTextWidget
+
+
+# Define a new FlatPageAdmin
+class FlatPageAdmin(FlatPageAdmin):
+    formfield_overrides = {
+        models.TextField: {'widget':RichTextWidget}
+    }
+
+
+# Re-register FlatPageAdmin
+admin.site.unregister(FlatPage)
+admin.site.register(FlatPage, FlatPageAdmin)
