3a07787df638f569794fc0ea09f5a3a87dd6a10e
dyou <chen.orange@aliyun.com>
2019-12-23 10:54:49 +0800

    update mce display

--- static/juss/widgets/richtext.js
+++ static/juss/widgets/richtext.js
@@ -3,5 +3,7 @@ tinymce.init({
     height: 600,
     language: 'zh_CN',
     language_url: '/static/bower_components/tinymce/zh_CN.js',
-    plugins: 'image lists'
+    plugins: 'image lists autolink fullscreen table media',
+    menubar: false,
+    toolbar: 'styleselect | bold italic forecolor backcolor | alignleft aligncenter alignright | numlist bullist | image media table | fullscreen'
 })
