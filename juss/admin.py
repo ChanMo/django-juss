ed66cb2f048c57810db34bbe54353c1b5021f713
dyou <chen.orange@aliyun.com>
2020-08-11 17:41:58 +0800

    add search_url

--- juss/admin.py
+++ juss/admin.py
@@ -3,8 +3,10 @@ from django.conf import settings
 
 class JussAdminSite(AdminSite):
     login_bg = None
+    search_url = None
 
     def each_context(self, request):
         context = super().each_context(request)
         context['login_bg'] = self.login_bg
+        context['search_url'] = self.search_url
         return context
